b6b4e1a8258aaea18b3b4feb1ed726d0c68f3f18
LeoneOdinga <leoneodinga41@gmail.com>
2021-12-26 17:12:39 +0300

    modified the client.py file

--- client.py
+++ client.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 #this is the client file
+
 import socket
 
-HOST = '127.0.0.1'
+HEADER =64
+FORMAT = 'utf-8'
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 56000
+ADDR = (HOST,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def sendMessage(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
-    while True:
-        print('Start a conversation:')
-        data1 = input()
-        if data1:
-            s.send(data1.encode())
-        else:
-            break
-        data = s.recv(1024)
-        if data :
-            print('Server Said:', repr(data))
-        else:
-            break
+    connection = True
+    while connection:
+       print("Please type a message you want to send to the server: ")
+       userInput = input()
+       sendMessage(userInput)
+
+sendMessage("Disconnected!")
